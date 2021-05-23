7dab27984f4e7c274a4501da29d017f3cc886576
Hesham-E <hesham.elkaliouby@ucalgary.ca>
2021-05-23 04:15:26 +0400

    Add support for all numerical types

---
+++ CWE-190 Integer Overflow or Wraparound/Testdata/test8.c
@@ -0,0 +1,12 @@
+int func ()
+{
+    int a = 1;
+    a += 2147483646;
+
+    long b = 1000;
+    unsigned short c = 1;
+    short o = 1;
+    unsigned long d = 10;
+    unsigned char z = 'a';
+    return 0;
+}
